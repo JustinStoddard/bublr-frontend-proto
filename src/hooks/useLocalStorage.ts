f088dd1ea4b9b69ec84e5ea062f5b25e95169d5f
Justin Stoddard <36201566+JustinStoddard@users.noreply.github.com>
2023-12-12 15:44:23 -0700

    Added ability to add bubbles and see them persist afterwards - saving bubble info into localstorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void, () => void] => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? (JSON.parse(item) as T) : initialValue;
+    } catch (error) {
+      console.error('Error fetching from localStorage:', error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T): void => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error('Error setting localStorage:', error);
+    }
+  };
+
+  const removeValue = (): void => {
+    try {
+      window.localStorage.removeItem(key);
+      setStoredValue(initialValue);
+    } catch (error) {
+      console.error('Error removing from localStorage:', error);
+    }
+  };
+
+  return [storedValue, setValue, removeValue];
+};
+
+export default useLocalStorage;
--- src/pages/BubblesPage/index.tsx
+++ src/pages/BubblesPage/index.tsx
@@ -1,46 +1,276 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import styles from "./styles.module.css";
 import PageContainer from "../../components/PageContainer";
 import { Add, ChevronLeft } from "@mui/icons-material";
 import { TextField } from "@mui/material";
-import mapboxgl from 'mapbox-gl';
+import mapboxgl, { Map, Marker, GeoJSONSource } from 'mapbox-gl';
+import useLocalStorage from "../../hooks/useLocalStorage";
  
 mapboxgl.accessToken = 'pk.eyJ1IjoianVzdGluLXN0b2RkYXJkIiwiYSI6ImNscTIxajJ4djAwdHgycnMyeW0yeXNzdG8ifQ.Fo2r-RxjpR8GJ7a6cq7gPg';
 
+type Bubble = {
+  bubbleId: string;
+  bubbleName: string;
+  bubbleLongitude: number;
+  bubbleLatitude: number;
+  bubbleRadius: number;
+};
+
 const BubblesPage = () => {
+  const [storedItem, setStoredItem] = useLocalStorage('bubbles', []);
   const [creatingBubble, setCreatingBubble] = useState<boolean>(false);
   const [buttonPillOpen, setButtonPillOpen] = useState<boolean>(true);
-  const [bubbleName, setBubbleName] = useState("");
+  const [showMarker, setShowMarker] = useState<boolean>(false);
   const [bubbleLongitude, setBubbleLongitude] = useState(-111.8855);
   const [bubbleLatitude, setBubbleLatitude] = useState(40.7623);
-  const mapContainer = useRef(null);
-  const map = useRef<mapboxgl.Map | null>(null);
+  const [bubbleName, setBubbleName] = useState("");
+  const [bubbleRadius, setBubbleRadius] = useState(0.5); //Radius in miles
+  const [map, setMap] = useState<Map | null>(null);
+  const [marker, setMarker] = useState<Marker | null>(null);
 
   useEffect(() => {
-    if (map.current && mapContainer.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current || "",
-      style: 'mapbox://styles/justin-stoddard/clq21l1m300bt01mrawpm5ijl',
-      center: [bubbleLongitude, bubbleLatitude],
-      zoom: 9
-    });
+    const initializeMap = () => {
+      const lng = bubbleLongitude;
+      const lat = bubbleLatitude;
+      const mapInstance = new Map({
+        container: "map",
+        style: 'mapbox://styles/justin-stoddard/clq21l1m300bt01mrawpm5ijl',
+        center: [lng, lat],
+        zoom: 12
+      });
+  
+      mapInstance.on("load", () => {
+        setMap(mapInstance);
+        addCircleLayer(mapInstance);
+        addMarker(mapInstance);
+        renderBubbles(mapInstance);
+        renderMarkers(mapInstance);
+      });
+    };
 
-    map.current.on('move', () => {
-      if (map.current !== null)  {
-        setBubbleLongitude(parseFloat(map.current.getCenter().lng.toFixed(4)));
-        setBubbleLatitude(parseFloat(map.current.getCenter().lat.toFixed(4)));
+    if (!map) {
+      initializeMap();
+    }
+  }, [map]);
+
+  useEffect(() => {
+    if (map) {
+      map.on("move", () => {
+        updateMarkerPosition();
+      });
+      map.on('zoom', () => {
+        updateCircleRadius(bubbleRadius);
+      });
+      // map.on('touchstart', () => {
+      //   setButtonPillOpen(false);
+      // });
+      // map.on('touchend', () => {
+      //   setButtonPillOpen(true);
+      // });
+    }
+  }, [map, bubbleRadius, creatingBubble]);
+
+  useEffect(() => {
+    updateCircleRadius(bubbleRadius);
+  }, [bubbleRadius, creatingBubble]);
+
+  useEffect(() => {
+    if (marker && map) {
+      if (showMarker) {
+        marker.addTo(map);
+        map.setPaintProperty('circle-layer-1', 'circle-opacity', 0.3);
+        map.setPaintProperty('circle-layer-1', 'circle-stroke-width', 2);
+      } else {
+        marker.remove();
+        map.setPaintProperty('circle-layer-1', 'circle-opacity', 0.0);
+        map.setPaintProperty('circle-layer-1', 'circle-stroke-width', 0);
+      }
+    }
+  }, [marker, map, showMarker]);
+
+  const renderBubbles = (mapInstance: Map) => {
+    const bubbles = storedItem as Bubble[];
+    bubbles.map(bubble => {
+      mapInstance.addSource(`circle-source-${bubble.bubbleId}`, {
+        type: 'geojson',
+        data: {
+          type: "Feature",
+          properties: null,
+          geometry: {
+            type: "Point",
+            coordinates: [bubble.bubbleLongitude, bubble.bubbleLatitude]
+          }
+        }
+      });
+
+      mapInstance.addLayer({
+        id: `circle-layer-${bubble.bubbleId}`,
+        type: "circle",
+        source: `circle-source-${bubble.bubbleId}`,
+        paint: {
+          'circle-radius': calculateRadius(mapInstance.getZoom(), bubble.bubbleRadius), // Radius in meters (adjust as needed)
+          'circle-color': '#006DAA', // Circle color
+          'circle-opacity': 0.3, // Circle opacity
+          'circle-stroke-color': '#006DAA',
+          'circle-stroke-width': 2,
+        },
+      });
+    });
+  };
+
+  const renderMarkers = (mapInstance: Map) => {
+    const bubbles = storedItem as Bubble[];
+    bubbles.map(bubble => {
+      new mapboxgl.Marker({ color: "#006DAA" }).setLngLat([bubble.bubbleLongitude, bubble.bubbleLatitude]).addTo(mapInstance).getElement().addEventListener('click', () => {
+        flyTo(mapInstance, bubble);
+      });
+    });
+  };
+
+  const flyTo = (mapInstance: Map, bubble: Bubble) => {
+    mapInstance.flyTo({
+      center: [bubble.bubbleLongitude, bubble.bubbleLatitude],
+      zoom: 12, // You can adjust the zoom level here
+      essential: true,
+    });
+  };
+
+  const renderBubble = (bubble: Bubble, mapInstance: Map) => {
+    mapInstance.addSource(`circle-source-${bubble.bubbleId}`, {
+      type: 'geojson',
+      data: {
+        type: "Feature",
+        properties: null,
+        geometry: {
+          type: "Point",
+          coordinates: [bubble.bubbleLongitude, bubble.bubbleLatitude]
+        }
       }
     });
-  });
+
+    mapInstance.addLayer({
+      id: `circle-layer-${bubble.bubbleId}`,
+      type: "circle",
+      source: `circle-source-${bubble.bubbleId}`,
+      paint: {
+        'circle-radius': calculateRadius(mapInstance.getZoom(), bubble.bubbleRadius), // Radius in meters (adjust as needed)
+        'circle-color': '#006DAA', // Circle color
+        'circle-opacity': 0.3, // Circle opacity
+        'circle-stroke-color': '#006DAA',
+        'circle-stroke-width': 2,
+      },
+    });
+    new mapboxgl.Marker({ color: "#006DAA" }).setLngLat([bubble.bubbleLongitude, bubble.bubbleLatitude]).addTo(mapInstance).getElement().addEventListener('click', () => {
+      flyTo(mapInstance, bubble);
+    });
+  };
+
+  const calculateRadius = (zoom: number, radius: number): number => {
+    const milesToMeters = 1609.34; // Conversion factor from miles to meters
+    const metersPerPixel = (40075016.686 / Math.pow(2, zoom)) / 256; // Earth's circumference / 2^zoom / 256
+    const meters = radius * milesToMeters;
+    return (meters / metersPerPixel) || 1; // Radius in pixels
+  };
+
+  const updateCircleRadius = (newRadius: number) => {
+    if (map) {
+      map.setPaintProperty('circle-layer-1', 'circle-radius', calculateRadius(map.getZoom(), newRadius));
+      const bubbles = storedItem as Bubble[];
+      bubbles.map(bubble => {
+        map.setPaintProperty(`circle-layer-${bubble.bubbleId}`, 'circle-radius', calculateRadius(map.getZoom(), bubble.bubbleRadius));
+      });
+    }
+  };
+
+  const updateMarkerPosition = () => {
+    if (marker && map) {
+      const center = map.getCenter();
+      marker.setLngLat(center);
+      setBubbleLongitude(center.lng);
+      setBubbleLatitude(center.lat);
+      (map.getSource('circle-source') as GeoJSONSource).setData({
+        type: 'Feature',
+        properties: null,
+        geometry: {
+          type: 'Point',
+          coordinates: [center.lng, center.lat],
+        },
+      });
+    }
+  };
+
+  const addMarker = (mapInstance: Map) => {
+    const lng = bubbleLongitude;
+    const lat = bubbleLatitude;
+    const newMarker = new mapboxgl.Marker({ color: "#006DAA" }).setLngLat([lng, lat]).addTo(mapInstance);
+    setMarker(newMarker);
+  };
+
+  const addCircleLayer = (mapInstance: Map) => {
+    const lng = bubbleLongitude;
+    const lat = bubbleLatitude;
+    mapInstance.addSource('circle-source', {
+      type: 'geojson',
+      data: {
+        type: "Feature",
+        properties: null,
+        geometry: {
+          type: "Point",
+          coordinates: [lng, lat]
+        }
+      }
+    });
+
+    mapInstance.addLayer({
+      id: 'circle-layer-1',
+      type: "circle",
+      source: "circle-source",
+      paint: {
+        'circle-radius': calculateRadius(mapInstance.getZoom(), bubbleRadius), // Radius in meters (adjust as needed)
+        'circle-color': '#006DAA', // Circle color
+        'circle-opacity': 0, // Circle opacity
+        'circle-stroke-color': '#006DAA',
+        'circle-stroke-width': 0,
+      },
+    });
+  };
 
   const createBubble = () => {
+    const bubbleId = uuidv4();
     console.log({
+      bubbleId,
       bubbleName,
       bubbleLongitude,
       bubbleLatitude,
+      bubbleRadius,
     });
+    const bubble: Bubble = {
+      bubbleId,
+      bubbleName,
+      bubbleLongitude,
+      bubbleLatitude,
+      bubbleRadius,
+    };
+    setStoredItem([...storedItem, bubble] as any);
     setCreatingBubble(false);
     setBubbleName("");
+    setShowMarker(false);
+    setButtonPillOpen(false);
+    setBubbleRadius(0.5);
+    renderBubble(bubble, map as Map);
+  };
+
+  const longitudeOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBubbleLongitude(e.currentTarget.valueAsNumber);
+  };
+
+  const latitudeOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBubbleLatitude(e.currentTarget.valueAsNumber);
+  };
+
+  const radiusOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBubbleRadius(e.currentTarget.valueAsNumber);
   };
 
   return (
@@ -62,13 +292,19 @@ const BubblesPage = () => {
           </div>
           <div
             className={`${styles.createBubbleButtonContainer} ${creatingBubble ? styles.bubbleButtonDisabled : ""}`}
-            onClick={() => !creatingBubble ? setCreatingBubble(true) : null}
+            onClick={() => {
+              if (!creatingBubble) {
+                setCreatingBubble(true);
+                setButtonPillOpen(true);
+                setShowMarker(true);
+              }
+            }}
           >
             <div className={`${styles.circle} ${creatingBubble ? styles.circleDisabled : ""}`} />
             <Add className={`${styles.addIcon} ${creatingBubble ? styles.addIconDisabled : ""}`} />
           </div>
         </div>
-        <div ref={mapContainer} className={styles.mapContainer} />
+        <div id="map" className={styles.mapContainer} />
         <div className={styles.footerContainer}>
           {creatingBubble && (
             <div className={styles.createBubblePillContainer}>
@@ -81,34 +317,50 @@ const BubblesPage = () => {
                 </div>
               </div>
               <div className={`${buttonPillOpen ? styles.bubblePillControlsContainerOpen : styles.bubblePillControlsContainerClosed}`}>
-                <TextField
-                  variant="outlined"
-                  label="Name"
-                  size="small"
-                  value={bubbleName}
-                  onChange={(e) => setBubbleName(e.target.value)}
-                  className={styles.input}
-                />
+                <div className={styles.upperInputsContainer}>
+                  <TextField
+                    variant="outlined"
+                    label="Name"
+                    size="small"
+                    value={bubbleName}
+                    onChange={(e) => setBubbleName(e.target.value)}
+                    className={styles.nameInput}
+                  />
+                  <TextField
+                    variant="outlined"
+                    label="Radius (mi)"
+                    size="small"
+                    type="number"
+                    value={bubbleRadius}
+                    onChange={radiusOnChange}
+                    className={styles.radiusInput}
+                  />
+                </div>
                 <TextField
                   variant="outlined"
                   label="Longitude"
                   size="small"
+                  type="number"
                   value={bubbleLongitude}
-                  onChange={(e) => setBubbleLongitude(parseFloat(e.target.value))}
+                  onChange={longitudeOnChange}
                   className={styles.input}
                 />
                 <TextField
                   variant="outlined"
                   label="Latitude"
                   size="small"
+                  type="number"
                   value={bubbleLatitude}
-                  onChange={(e) => setBubbleLatitude(parseFloat(e.target.value))}
+                  onChange={latitudeOnChange}
                   className={styles.input}
                 />
                 <div className={styles.buttonsContainer}>
                   <div
                     className={`${styles.button} ${styles.cancel}`}
-                    onClick={() => setCreatingBubble(false)}
+                    onClick={() => {
+                      setCreatingBubble(false);
+                      setShowMarker(false);
+                    }}
                   >
                     Cancel
                   </div>
