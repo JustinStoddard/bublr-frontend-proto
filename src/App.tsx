358cc82840eb4b47b93ffbb2c8a4d45acd899d34
Justin Stoddard <36201566+JustinStoddard@users.noreply.github.com>
2023-12-11 20:06:01 -0700

    added some initial files and comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import PageLoader from "./components/Loader";
+
+const HomePage = lazy(() => import('./pages/HomePage'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Suspense fallback={<PageLoader />}>
+          <Route path="/" element={<HomePage />} />
+        </Suspense>
+      </Routes>
+    </Router>
   );
 }
 
